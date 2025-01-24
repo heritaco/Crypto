16b5b26f01c4f644e2588d108e47ea8044320224
CaracolDelDesierto <141775261+CaracolDelDesierto@users.noreply.github.com>
2025-01-24 17:53:28 -0600

    Update Coin.py
    :3

--- 07-AnalysisPackage/Coin.py
+++ 07-AnalysisPackage/Coin.py
@@ -1,6 +1,8 @@
 from pandas import (
     DataFrame,
-    to_datetime
+    to_datetime,
+    read_csv,
+    concat
 )
 
 import numpy as np
@@ -16,26 +18,26 @@ import os
 
 class Coin:
     """
-    Description:
-    A Class containing the historical data of the prices for a coin, along with
-    the profit. It also allows the prediction of the prices through some
-    simulation algorithms.
+    A Class containing the historical data of the prices for a coin, along
+    with the profit. It also allows the prediction of the prices through
+    some simulation algorithms.
 
 
-    Parameters:
-    token : str
-        The corresponding token that will be sent to the Binance database to
-        download the historical data of the prices of the coin.
+    :parameter:
+        token : str
+        The corresponding token that will be sent to the Binance database
+        to download the historical data of the prices of the coin.
+        It is formed as Forex nicknames, e.g.: BTCUSDT, ETHUSDT.
 
-    interval : str
-        The length of each entry of the coin historical data. Must be one of the
-        followings:
-        - Seconds:  1  s
-        - Minutes:  1  m / 3  m / 5  m / 15 m / 30 m
-        - Hours:    1  h / 2  h / 4  h / 6  h / 8  h / 12 h
-        - Days:     1  d / 3  d
-        - Weeks:    1  w
-        - Months:   1  M
+        interval : str
+        The length of each entry of the coin historical data. Must be one
+        the followings:
+        - Seconds:    1s
+        - Minutes:    1m /   3m /   5m /  15m /  30m
+        - Hours:      1h /   2h /   4h /   6h /   8h /  12h
+        - Days:       1d /   3d
+        - Weeks:      1w
+        - Months:     1M
 
     Notes:
     Please make sure to check the Binance database for available coins!
@@ -49,6 +51,7 @@ class Coin:
 
         self.token = token
         self.data = download_data(token, interval)
+        self.interval = interval
 
         # We adjust the time as the corresponding
 
@@ -67,85 +70,146 @@ class Coin:
         self.data["taker_base_vol"] = self.data["taker_base_vol"].apply(lambda x: float(x))
         self.data["taker_quote_vol"] = self.data["taker_quote_vol"].apply(lambda x: float(x))
 
-        # We add the profit and accumulated profit since data was downloaded
+        # We add the net profit and max profit
 
-        self.data["profit"] = 100 * (self.data["c"] / self.data["c"].shift(1) - 1)
-        self.data["accum_profit"] = (1 + self.data["profit"] / 100).cumprod() - 1
-        self.data["accum_profit"] = self.data["accum_profit"] * 100
+        self.data["net_profit"] = 100 * (self.data["c"] / self.data["c"].shift(1) - 1)
+        self.data["max_profit"] = 100 * (self.data["o"] / self.data["h"] - 1)
 
         # ADDING INDICATORS #
-        # Moving Averages
-
-        self.data["short_ma"] = self.data["c"].rolling(window=3, min_periods=1).mean()
-        self.data["long_ma"] = self.data["c"].rolling(window=15, min_periods=1).mean()
-        self.data["ma_diff"] = self.data["short_ma"] - self.data["long_ma"]
 
         # Buy / Sell Volume Difference
 
         self.data["buy_usdt_vol"] = self.data["taker_quote_vol"]
         self.data["sell_usdt_vol"] = self.data["qav"] - self.data["taker_quote_vol"]
-        self.data["buy_sell_vol_ratio"] = self.data["buy_usdt_vol"] - self.data["sell_usdt_vol"]
+        self.data["buy_sell_vol_diff"] = self.data["buy_usdt_vol"] - self.data["sell_usdt_vol"]
 
         # Volume Difference Moving Average
 
-        self.data["average_vol"] = self.data["buy_sell_vol_ratio"].rolling(window=3, min_periods=1).mean()
+        self.data["average_vol"] = self.data["buy_sell_vol_diff"].rolling(window=3, min_periods=1).mean()
 
         # On Board Volume
 
         self.data["obv"] = 0
-        self.data["obv"] = self.data["obv"].shift(1) + np.sign(self.data["profit"]) * self.data["qav"]
-
-        # Relative Strength Index
-
-        delta = self.data["c"].diff()
-        gain = delta.where(delta > 0, 0)
-        loss = -delta.where(delta < 0, 0)
-        avg_gain = gain.rolling(window=3, min_periods=1, closed="left").mean()
-        avg_loss = loss.rolling(window=3, min_periods=1, closed="left").mean()
-        rs = avg_gain / avg_loss
-        self.data["rsi"] = 100 - (100 / (1 + rs))
+        self.data["obv"] = self.data["obv"].shift(1) + np.sign(self.data["net_profit"]) * self.data["qav"]
 
     def __str__(self):
         return self.token
 
     def copy_data(self):
+        """
+        This method returns a shallow copy of the historical data that can be modified
+        without affecting the data atribute of the object.
+
+        :parameter:
+            none
+
+        :returns:
+            data_copy : pandas.DataFrame
+            A shallow copy of the coin historical data. Changes done to the copy will
+            not affect the data attribute of the object.
+        """
+
         return self.data.copy(deep=False)
 
     def print_data(self):
+        """
+        This method prints the historical data of the coin directly into the console
+        as a table.
+
+        :parameter:
+            none
+
+        :returns:
+            none
+        """
+
         print(tabulate(self.data, headers="keys"))
 
     def export_data(self, file_path):
+        """
+        This method saves the historical data of the coin into a csv file. If the
+        corresponding file already exists into the path, the new data that is not
+        already in the file will be added.
+
+        :parameter:
+            file_path : str
+            The path to the file where the data will be saved.
+
+        :returns:
+            none
+        """
+
+        file_name = f"{file_path}/{self.token}_{self.interval}.csv"
+
+        # Remove the last tuple of the database, as it has incomplete data
+
+        data_copy = self.data.iloc[:-1, :]
+
+        # If the path does not exist, it is created
+
         if not os.path.exists(file_path):
             os.makedirs(file_path)
 
-        filename = f"{file_path}/{self.token}.csv"
-        self.data.to_csv(filename)
+        # If the file already exists, it is update, otherwise, it is created
+
+        if os.path.exists(file_name):
+            data_copy = self.update_data(file_path)
+
+        data_copy.to_csv(file_name)
+
+    def update_data(self, file_path):
+        """
+        This method takes the historical data of the coin that is already saved
+        in the path and updates it with the new data.
+
+        :parameter:
+            file_path : str
+            The path to the file where the data is already saved.
+
+        :returns:
+            final_data : pandas.DataFrame
+            The updated DataFrame.
+        """
+
+        saved_data = read_csv(f"{file_path}/{self.token}_{self.interval}.csv", index_col=0)
+        new_data = self.copy_data()
+
+        saved_data.index = to_datetime(saved_data.index)
+        saved_index = set(saved_data.index)
+        new_data = new_data.loc[~new_data.index.isin(saved_index)]
+        final_data = concat([saved_data, new_data])
+
+        return final_data
 
 
 def download_data(token, interval):
     """
-    Description:
     This function sends a request to the Binance API and then returns the data
     as a JSON object. The data is transformed into a DataFrame and sent to the
     Coin object. The parameters are those sent directly to the Coin
     constructor method.
 
 
-    Parameters:
-    token : str
+    :parameter:
+        token : str
         The corresponding token that will be sent to the Binance database to
         download the historical data of the prices of the coin.
 
-    interval : str
+        interval : str
         The length of each entry of the coin historical data. Must be one of the
         followings:
-        - Seconds:  1  s
-        - Minutes:  1  m / 3  m / 5  m / 15 m / 30 m
-        - Hours:    1  h / 2  h / 4  h / 6  h / 8  h / 12 h
-        - Days:     1  d / 3  d
-        - Weeks:    1  w
-        - Months:   1  M
+        - Seconds:    1s
+        - Minutes:    1m /   3m /   5m /  15m /  30m
+        - Hours:      1h /   2h /   4h /   6h /   8h /  12h
+        - Days:       1d /   3d
+        - Weeks:      1w
+        - Months:     1M
 
+
+    :returns:
+        historical_data : pandas.DataFrame
+        The basic historical data of the prices of the coin, along with the
+        corresponding volume.
 
     Taken from:
     https://stackoverflow.com/questions/66295187/how-do-i-get-all-the-prices-history-with-binance-api-for-a-crypto-using-python
@@ -155,11 +219,11 @@ def download_data(token, interval):
     url = root_url + '?symbol=' + token + '&interval=' + interval
 
     data = json.loads(requests.get(url).text)
-    df = DataFrame(data)
-    df.columns = ['open_time',
+    historical_data = DataFrame(data)
+    historical_data.columns = ['open_time',
                   'o', 'h', 'l', 'c', 'v',
                   'close_time', 'qav', 'num_trades',
                   'taker_base_vol', 'taker_quote_vol', 'ignore']
-    df.index = [dt.datetime.fromtimestamp(x / 1000.0) for x in df.open_time]
+    historical_data.index = [dt.datetime.fromtimestamp(x / 1000.0) for x in historical_data.open_time]
 
-    return df
+    return historical_data
